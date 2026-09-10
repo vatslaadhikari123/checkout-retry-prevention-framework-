45af97beb7c30fbb12dfecda61aa5b44661b2c28
vatslaadhikari123 <vatslaadhikari123@gmail.com>
2026-09-10 21:44:40 +0530

    Update RetryPreventionModule.java

--- src/main/java/com/vatsla/retry/module/RetryPreventionModule.java
+++ src/main/java/com/vatsla/retry/module/RetryPreventionModule.java
@@ -1,32 +1,32 @@
-package com.alnt.jobserver.retry.prevention.module;
+package com.vatsla.retry.module;
 
-import com.alnt.jobserver.platform.base.retry.busobj.annotation.CheckBusObjWithMaxAttempts;
-import com.alnt.jobserver.platform.base.retry.busobj.annotation.FilterBusObjWithMaxAttempts;
-import com.alnt.jobserver.platform.base.retry.busobj.annotation.UpdateBusObjRetryAttempts;
-import com.alnt.jobserver.platform.base.retry.busobj.aspects.BusObjProactivePreventiveRetryAspect;
-import com.alnt.jobserver.platform.base.retry.busobj.aspects.BusObjReactivePreventiveRetryAspect;
-import com.alnt.jobserver.platform.base.retry.busobj.aspects.BusObjUpdateRetryAttemptsAspect;
-import com.alnt.platform.application.injector.annotation.Module;
 import com.google.inject.AbstractModule;
 import com.google.inject.matcher.Matchers;
+import com.vatsla.retry.annotation.FilterBusObjWithMaxAttempts;
+import com.vatsla.retry.annotation.UpdateBusObjRetryAttempts;
+import com.vatsla.retry.aspect.BusObjProactivePreventiveRetryAspect;
+import com.vatsla.retry.aspect.BusObjUpdateRetryAttemptsAspect;
 
-@Module
 public class RetryPreventionModule extends AbstractModule {
+
     @Override
     protected void configure() {
-        BusObjProactivePreventiveRetryAspect proactivePreventiveRetryAspect = new BusObjProactivePreventiveRetryAspect();
-        requestInjection(proactivePreventiveRetryAspect);
-        bindInterceptor(Matchers.any(), Matchers.annotatedWith(FilterBusObjWithMaxAttempts.class),
-                proactivePreventiveRetryAspect);
+        // Phase 1 Aspect Registration
+        BusObjProactivePreventiveRetryAspect proactiveAspect = new BusObjProactivePreventiveRetryAspect();
+        requestInjection(proactiveAspect);
+        bindInterceptor(
+                Matchers.any(),
+                Matchers.annotatedWith(FilterBusObjWithMaxAttempts.class),
+                proactiveAspect
+        );
 
-        BusObjReactivePreventiveRetryAspect reactivePreventiveRetryAspect = new BusObjReactivePreventiveRetryAspect();
-        requestInjection(reactivePreventiveRetryAspect);
-        bindInterceptor(Matchers.any(), Matchers.annotatedWith(CheckBusObjWithMaxAttempts.class),
-                reactivePreventiveRetryAspect);
-
-        BusObjUpdateRetryAttemptsAspect updateRetryAttemptsAspect = new BusObjUpdateRetryAttemptsAspect();
-        requestInjection(updateRetryAttemptsAspect);
-        bindInterceptor(Matchers.any(), Matchers.annotatedWith(UpdateBusObjRetryAttempts.class),
-                updateRetryAttemptsAspect);
+        // Phase 2 Aspect Registration
+        BusObjUpdateRetryAttemptsAspect updateAspect = new BusObjUpdateRetryAttemptsAspect();
+        requestInjection(updateAspect);
+        bindInterceptor(
+                Matchers.any(),
+                Matchers.annotatedWith(UpdateBusObjRetryAttempts.class),
+                updateAspect
+        );
     }
 }
