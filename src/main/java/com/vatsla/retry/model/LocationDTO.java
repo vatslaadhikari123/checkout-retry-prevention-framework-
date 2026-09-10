39fad55a6fe9575a19ced6354d2a83cf67e492d8
vatslaadhikari123 <vatslaadhikari123@gmail.com>
2026-09-10 21:36:17 +0530

    Create LocationDTO.java

---
+++ src/main/java/com/vatsla/retry/model/LocationDTO.java
@@ -0,0 +1,17 @@
+package com.vatsla.retry.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class LocationDTO extends BaseDTO {
+    private String locationCode;
+    private String locationName;
+}
